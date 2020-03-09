57a4b2b67a95b0d607b54f10f8cceca2828d2609
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-09 19:27:02 -0300

    mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import {API_CONFIG} from'../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -22,10 +24,13 @@ export class CategoriasPage {
     public categoriaService : CategoriaService) {
   }
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
+
   ionViewDidLoad() {
     this.categoriaService.findAll()
     .subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
     error =>{
       console.log(error);
