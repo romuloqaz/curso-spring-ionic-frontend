677b5927b32ed5f5bf339fb7e07c07db496788aa
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-09 19:54:02 -0300

    criando interceptor para tratamento de erro

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -32,11 +32,6 @@ export class CategoriasPage {
     .subscribe(response => {
       this.items = response;
     },
-    error =>{
-      console.log(error);
-    }
-      )
-    console.log();
+    error =>{});
   }
-
 }
