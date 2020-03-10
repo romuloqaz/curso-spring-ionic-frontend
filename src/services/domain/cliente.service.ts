7fa5eec43458ce358701ba99601f8130958500cb
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-10 17:02:19 -0300

    buscando endereco do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,10 +11,10 @@ export class ClienteService{
 
   }
 
-  findByEmail(email: string) : Observable<ClienteDTO> {
-    return this.http.get<ClienteDTO>(
-        `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+  findByEmail(email: string){
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
+
     getImageFromBucket(id : string) : Observable<any> {
       let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
       return this.http.get(url, {responseType : 'blob'});
