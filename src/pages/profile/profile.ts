4f85cf7553cb4b396f9a6c7e48fd2e3b65a74ee6
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-10 20:01:16 -0300

    usando cordova para tirar foto com a camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -12,28 +13,31 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProfilePage {
 
-  cliente : ClienteDTO
+  cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService : ClienteService) {
+    public clienteService: ClienteService,
+    public camera: Camera) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.cliente = response as ClienteDTO;// casting ClienteDTO
-        this.getImageIfExists();
-      },
-      error => {
-        if (error.status == 403){
-          this.navCtrl.setRoot('HomePage');
-        }
-      });
+        .subscribe(response => {
+          this.cliente = response as ClienteDTO;
+          this.getImageIfExists();
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
     }
     else {
       this.navCtrl.setRoot('HomePage');
@@ -48,5 +52,21 @@ export class ProfilePage {
     error => {});
   }
 
-}
+  getCameraPicture() {
 
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
+  }
+}
