ac4ee98ceb79f76f7cb4c061d3f25c4a1c29f02f
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-10 10:09:40 -0300

    tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,9 +29,17 @@ export class ProfilePage {
         this.cliente = response;
         this.getImageIfExists();
       },
-      error => {});
+      error => {
+        if (error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
+
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,5 +1,5 @@
 import { StorageService } from "../storage.service";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
