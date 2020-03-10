e60a46ce449b8b3566bfcbbf522364af91a4133b
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-10 18:15:53 -0300

    tela de confirmacao de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -33,7 +33,7 @@ export class PaymentPage {
 
   nextPage() {
     this.pedido.pagamento = this.formGroup.value;
-    console.log(this.pedido);
+    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido}); //passando pedido na pag
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -29,5 +29,8 @@ export class ClienteService{
             responseType: 'text'
         }
     );
-}
+  }
+  findById(id: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+  }
 }
