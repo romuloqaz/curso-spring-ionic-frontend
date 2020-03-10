8073cee2374662ff6b013426e01d1d92b563a497
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-10 12:38:13 -0300

    salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fildemessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -35,6 +36,10 @@ export class ErrorInterceptor implements HttpInterceptor {
               this.handle403();
               break;
 
+              case 422:
+                this.handle422(errorObj);
+                break;
+
               default:
               this.handleDefaultError(errorObj);
             }
@@ -60,6 +65,21 @@ export class ErrorInterceptor implements HttpInterceptor {
       });
       alert.present();
     }
+
+    handle422(errorObj) {
+      let alert = this.alertCtrl.create({
+          title: 'Erro 422: Validação',
+          message: this.listErrors(errorObj.errors), //lista de error do valitadion error back
+          enableBackdropDismiss: false,
+          buttons: [
+              {
+                  text: 'Ok'
+              }
+          ]
+      });
+      alert.present();
+  }
+
     handleDefaultError(errorObj){
       let alert = this.alertCtrl.create({
         title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
@@ -73,9 +93,15 @@ export class ErrorInterceptor implements HttpInterceptor {
     });
     alert.present();
     }
+
+    private listErrors(messages : FieldMessage[]) : string {
+      let s : string = '';
+      for (var i=0; i<messages.length; i++) {
+          s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+      }
+      return s;
+  }
 }
-
-
 
 export const ErrorInterceptorProvider = {
     provide: HTTP_INTERCEPTORS,
